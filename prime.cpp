0d1a86358305e82de74b62719027d6f92a1d12c7
cool-nerdy <33223406+cool-nerdy@users.noreply.github.com>
2017-10-30 17:59:07 +0530

    Add files via upload
    added a prime number program in c

--- prime.cpp
+++ prime.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
 int main()
 {
-  int n, i;
-  bool isPrime = true;
+    int n, i, flag = 0;
 
-  cout << "Enter a positive integer: ";
-  cin >> n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
 
-  for(i = 2; i <= n / 2; ++i)
-  {
-      if(n % i == 0)
-      {
-          isPrime = false;
-          break;
-      }
-  }
-  if (isPrime)
-      cout << "This is a prime number";
-  else
-      cout << "This is not a prime number";
+    for(i=2; i<=n/2; ++i)
+    {
+        // condition for nonprime number
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
 
-  return 0;
+    if (flag==0)
+        printf("%d is a prime number.",n);
+    else
+        printf("%d is not a prime number.",n);
+    
+    return 0;
 }
